a230dbf4cc4b488a26cab049f7967f10456d5540
Lavanya Kommaraju <70351093+Lavanyamca22@users.noreply.github.com>
2021-05-30 15:50:06 +0530

    Create Calender_Module.py
    Sample Input
    
    08 05 2015
    Sample Output
    
    WEDNESDAY
    Explanation
    
    The day on August 5th 2015 was WEDNESDAY.

---
+++ Calender_Module.py
@@ -0,0 +1,12 @@
+import datetime 
+from datetime import date
+import calendar
+  
+def findDay(date):
+    month, day, year = (int(i) for i in date.split(' '))    
+    born = datetime.date(year, month, day)
+    return born.strftime("%A")
+  
+# Driver program
+date = input()
+print(findDay(date))
